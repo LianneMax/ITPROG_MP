f3a00de7e882536f8c3cef008215f1afa2cd0fbb
jeremiahmaxwellang <1jma7fltcw@gmail.com>
2024-10-01 23:22:38 +0800

    returned comments

--- EnrollmentPage.php
+++ EnrollmentPage.php
@@ -1,3 +1,19 @@
 <?php
-// commet
+// PHASE 1  - Client Side
+
+// LOGIN PAGE                              ==>
+// COURSE OFFERINGS PAGE ==>
+// ENROLLMENT PAGE              ==>
+// - Functionalities:
+// - Add/drop class
+// - Checkout and confirm
+// VIEW EAF                                   ==>
+
+
+
+// PHASE 2 - Server Side
+
+// ADMIN LOGIN PAGE                  ==>
+// CLASS MANAGEMENT PAGE ==>
+// SUMMARY REPORT PAGE       ==>
 ?>
