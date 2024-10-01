4aaf09bdf7eabdc6679788c37b82b37216c21316
jeremiahmaxwellang <1jma7fltcw@gmail.com>
2024-10-01 23:15:56 +0800

    phase 1 and 2 comments

--- EnrollmentPage.php
+++ EnrollmentPage.php
@@ -1,3 +1,19 @@
 <?php
-// you know who else doesnt know how to use github? my mom
+// **PHASE 1  - Client Side**
+
+// LOGIN PAGE                              ==>
+// COURSE OFFERINGS PAGE ==>
+// ENROLLMENT PAGE              ==>
+// - Functionalities:
+// - Add/drop class
+// - Checkout and confirm
+// VIEW EAF                                   ==>
+
+
+
+// **PHASE 2 - Server Side**
+
+// ADMIN LOGIN PAGE                  ==>
+// CLASS MANAGEMENT PAGE ==>
+// SUMMARY REPORT PAGE       ==>
 ?>
