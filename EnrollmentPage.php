8a6451c986ae5f9e78e37faf1a5952d31983c426
LianneMax <165748673+LianneMax@users.noreply.github.com>
2024-10-01 23:30:47 +0800

    This Marks the Start of the MP
    Comments for Phases

--- EnrollmentPage.php
+++ EnrollmentPage.php
@@ -9,15 +9,10 @@
 // - Checkout and confirm
 // VIEW EAF                                   ==>
 
-//HELLO JUSTIN
-
-//hello, did you know that in terms of pokemon and human breedint, vaporeon is the most compatible pokemon!
-
 // PHASE 2 - Server Side
 
 // ADMIN LOGIN PAGE                  ==>
 // CLASS MANAGEMENT PAGE ==>
 // SUMMARY REPORT PAGE       ==>
 
-// This is max - Hello
 ?>
