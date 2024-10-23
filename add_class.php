84ea03c861540edbdb39e2a4f75fbc2642ec359c
jeremiahmaxwellang <1jma7fltcw@gmail.com>
2024-10-23 12:13:51 +0800

    Created style.css
    All style elements are to be placed here from now on
    
    Pages that haven't referenced style.css yet:
    
    ViewEAF
    LoginPage

--- add_class.php
+++ add_class.php
@@ -1,86 +1,7 @@
 <html>
 <head>
     <title>Add Classes</title>
-    <!-- put template in 1 css file -->
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap');
-
-        body {
-            font-family: 'Montserrat', Times New Roman, serif;
-            background-color: #f4f4f4;
-            margin: 0;
-            padding: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            background-color: #f0f0f0;
-            position: relative;
-        }
-
-        .itmosys-header {
-            position: absolute;
-            top: 10px; 
-            font-size: 36px;
-            font-family: fantasy;
-            color: #5b8ba4; 
-        }
-
-        .container {
-            position: relative;
-            text-align: left;
-            background-color: white;
-            padding: 30px;
-            border-radius: 10px;
-            box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.1);
-            width: 1000px; 
-            /* consistent across all pages */
-            height: 500px;
-            margin-top: 40px;
-        }
-
-        h2 {
-            margin: 0;
-            padding-bottom: 10px;
-            color: #333;
-            font-family: 'Montserrat', Times New Roman, serif;
-        }
-
-        .add-class-header {
-            position: absolute;
-            top: 20px;
-            left: 20px;
-            font-family: 'Montserrat', Times New Roman, serif;
-            color: #333;
-            font-size: 24px;
-        }
-
-        /* Line separator placed directly below Enrollment */
-        .separator {
-            border-top: 2px solid #ccc;
-            margin-top: 40px; 
-            margin-bottom: 20px; 
-        }
-
-        button {
-            display: block;
-            width: 100%;
-            padding: 15px;
-            margin: 10px 0;
-            font-size: 16px;
-            font-family: "Times New Roman", serif; 
-            border: none;
-            border-radius: 5px;
-            background-color: #5b8ba4;
-            color: white;
-            cursor: pointer;
-            transition: background-color 0.3s ease;
-        }
-
-        button:hover {
-            background-color: #497285;
-        }
-    </style>
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
 
@@ -89,7 +10,7 @@
 
     <div class="container">
         <!-- Enrollment header in the top-left of the box -->
-        <h2 class="add-class-header">Add/Drop Classes Facility</h2>
+        <h2 class="title-header">Add/Drop Classes Facility</h2>
 
         <!-- Line below the Enrollment header -->
         <div class="separator"></div>
@@ -101,7 +22,7 @@
             
             <?php
             // error_reporting(E_ERROR | E_PARSE);
-            echo $_GET['offeringCode'];
+            // echo $_GET['offeringCode'];
             ?>
             </form>
         </div>
